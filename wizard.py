c0850dc30932e34884e715643ebf2d55afa30567
Jam Risser <jam@jamrizzi.com>
2016-08-21 14:35:45 -0500

    localized python

--- wizard.py
+++ wizard.py
@@ -1,41 +1,21 @@
 import os
 import sys
-from uni import Uni
+import yaml
 from profile import Profile
 
 class Wizard:
     
-    def __init__(self):
-        global uni
-        uni = Uni()
+    def __init__(self, wizard):
         self.profile = Profile()
-
-    def chooseDeveloperType(self):
-        print '[0] Backend'
-        print '[1] Front-end'
-        print '[2] Full-stack'
-        print '[3] Hacker'
-        self.profile.developerType = uni.read('Select what kind of developer you are: ')
-
-    def chooseProgrammingTechnologies(self):
-        if self.profile.developerType == 'Backend':
-            print '[0] Java'
-            print '[1] Python'
-            print '[2] Go'
-            print '[3] JavaScript'
-            print '[4] PHP'
-        else if self.profile.developerType == 'Front-end':
-            print '[0] JavaScript'
-            print '[1] ReactJS'
-            print '[2] AngularJS'
-            print '[3] Polymer'
-        else:
+        with open(wizard) as stream:
+            try:
+                self.wizard = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
         
-        self.profile.technologies = uni.read('Select the technologies you use:')
-        
-    def install(self):
-        print self.profile.developerType
-        
-wizard = Wizard()
-wizard.chooseDeveloperType()
-wizard.install()
+    def run(self):
+        print('im running')
+        print(self.wizard)
+    
+wizard = Wizard('wizard.yml')
+wizard.run()
