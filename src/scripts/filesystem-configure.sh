c7fe9d4f96d1a44fbf87cf7087206b195907cf8d
Jam Risser <jam@codejam.ninja>
2018-07-23 19:47:23 +0530

    Added more packages

--- src/scripts/filesystem-configure.sh
+++ src/scripts/filesystem-configure.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# repos
+echo | add-apt-repository ppa:codejamninja/jam-os
+
 # setup
 cd /root
 apt-get install -y \
@@ -21,3 +24,9 @@ pip3 install virtualenv
 # i3pystatus
 pip3 install git+https://github.com/enkore/i3pystatus.git
 
+# oh my zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git /etc/skel/.oh-my-zsh
+git clone https://github.com/denysdovhan/spaceship-prompt.git \
+    /etc/skel/.oh-my-zsh/custom/themes/spaceship-prompt
+ln -s /etc/skel/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme \
+   /etc/skel/.oh-my-zsh/custom/themes/spaceship.zsh-theme
