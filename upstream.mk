6fc3584eea7740054cbfbb19cc120dacdc80c54e
Clay Risser <clayrisser@gmail.com>
2022-01-08 07:45:06 -0600

    Updated sway config

--- upstream.mk
+++ upstream.mk
@@ -1,16 +1,10 @@
 export SWAY_VERSION ?= 1.5.1
 
-export UPSTREAM := config-overrides/includes.chroot/etc/skel/.config/sway/config \
-	config-overrides/includes.installer/preseed.cfg
+export UPSTREAM := config-overrides/includes.installer/preseed.cfg
 
 export CURL ?= curl
 export PATCHES := $(UPSTREAM)
 include $(MKPM)/patch
-
-config-overrides/includes.chroot/etc/skel/.config/sway/config:
-	@$(MKDIR) -p $(@D)
-	@$(CURL) -Lo $@ \
-		https://raw.githubusercontent.com/swaywm/sway/$(SWAY_VERSION)/config.in
 
 config-overrides/includes.installer/preseed.cfg:
 	@$(MKDIR) -p $(@D)
