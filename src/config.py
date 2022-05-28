c8d2a2ca35a4e129ab872c72522530f7593f337e
Clay Risser <clayrisser@gmail.com>
2022-05-28 06:13:08 -0500

    Improved prepare

--- src/config.py
+++ src/config.py
@@ -1,6 +1,21 @@
+import os
+
+
 class Config:
     def __init__(self, config):
         self.config = config
+
+    @property
+    def paths(self):
+        root_path = os.path.realpath(
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+        )
+        build_path = os.path.join(root_path, ".build")
+        return {
+            "root": root_path,
+            "build": build_path,
+            "lb": os.path.join(build_path, "lb"),
+        }
 
     @staticmethod
     async def create(config={}):
--- src/stages/prepare.py
+++ src/stages/prepare.py
@@ -1,6 +1,29 @@
+import os
+import shutil
+
+
 class PrepareStage:
     def __init__(self, config):
         self.config = config
 
-    async def run(self) -> None:
+    async def run(self):
+        await self.initialize()
         print("preparing")
+
+    async def initialize(self):
+        if not os.path.exists(self.config.paths["build"]):
+            os.makedirs(self.config.paths["build"])
+        shutil.copyfile(
+            os.path.join(self.config.paths["root"], "mkpm.mk"),
+            os.path.join(self.config.paths["build"], "mkpm.mk")
+        )
+        if os.path.exists(os.path.join(self.config.paths["root"], ".mkpm")):
+            shutil.copytree(
+                os.path.join(self.config.paths["root"], ".mkpm"),
+                os.path.join(self.config.paths["build"], ".mkpm")
+            )
+        if os.path.exists(self.config.paths["lb"]):
+            shutil.rmtree(self.config.paths["lb"])
+        shutil.copytree(
+            os.path.join(self.config.paths["root"], "lb"), self.config.paths["lb"]
+        )
