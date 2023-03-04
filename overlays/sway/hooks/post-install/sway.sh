44a50829967d0a7c520d81c9e23918c744c2ac39
Clay Risser <clayrisser@gmail.com>
2023-03-03 22:06:56 -0600

    Fixed bugs

--- overlays/sway/hooks/post-install/sway.sh
+++ overlays/sway/hooks/post-install/sway.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-echo apt-mark hold sway
 apt-mark hold sway
 
-echo pip3 install autotiling
 pip3 install autotiling
+
+if (cat /etc/gdm3/daemon.conf | grep -qE '^DefaultSession='); then
+    sed -i 's|^\(DefaultSession=\).*|\1sway|g' /etc/gdm3/daemon.conf
+else
+    sed -i "$(grep -n '\[daemon\]' /etc/gdm3/daemon.conf | cut -d ':' -f1) a DefaultSession=sway" /etc/gdm3/daemon.conf
+fi
