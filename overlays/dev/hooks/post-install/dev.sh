2d2ee6325269457dd1b9fecb55fa2023a09b3401
Clay Risser <clayrisser@gmail.com>
2023-03-16 04:22:36 -0400

    Improved install

--- overlays/dev/hooks/post-install/dev.sh
+++ overlays/dev/hooks/post-install/dev.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-if (echo $PATH | grep -vq '/usr/local/bin'); then
-    export PATH="/usr/local/bin:$PATH"
-fi
-
-$(curl --version >/dev/null 2>/dev/null && echo curl -L || echo wget -O-) \
-    https://gitlab.com/risserlabs/community/cody/-/raw/main/cody.sh 2>/dev/null | sh -s i cody
-cody install cinch
-cody install dotstow
-
 if [ -f /etc/skel/.config/sway/startup.sh ]; then
     cat <<EOF >> /etc/skel/.config/sway/startup.sh
 
