3451dbf901e01cad174b4f97a7c1998499c16854
Clay Risser <clayrisser@gmail.com>
2023-03-17 08:38:25 -0400

    Improved debug

--- overlays/grub/hooks/lb-overlay.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -d config/includes.installer/root/install/filesystem/boot/grub/themes/default ]; then
-    mkdir -p config/includes.binary/boot/grub
-    rm -rf config/includes.binary/boot/grub/live-theme
-    cp -r config/includes.installer/root/install/filesystem/boot/grub/themes/default \
-        config/includes.binary/boot/grub/live-theme
-fi
